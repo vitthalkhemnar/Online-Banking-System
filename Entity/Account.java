c9efaebd616a87fde5f2a3ed34af2a22e1c26abf
Vitthal Khemnar <121882018+vitthalkhemnar@users.noreply.github.com>
2024-03-16 10:24:42 +0530

    Add files via upload

---
+++ Entity/Account.java
@@ -0,0 +1,83 @@
+package com.entity;
+
+public class Account {
+
+	private String fname;
+	private String lname;
+	private String phone;
+	private String email;
+	private String address;
+	private String dob;
+	private String gender;
+	private String mar;
+	private String accno;
+	private String ifsc;
+	private String branch;
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMar() {
+		return mar;
+	}
+	public void setMar(String mar) {
+		this.mar = mar;
+	}
+	public String getAccno() {
+		return accno;
+	}
+	public void setAccno(String accno) {
+		this.accno = accno;
+	}
+	public String getIfsc() {
+		return ifsc;
+	}
+	public void setIfsc(String ifsc) {
+		this.ifsc = ifsc;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+}
